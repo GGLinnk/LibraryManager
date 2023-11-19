f2be7b4c88fad807ea210ad93ad097ffc7d15669
Gabriel GRONDIN <gglinnk@protonmail.com>
2023-11-19 23:22:11 +0100

    Fixed LibraryItem logic

--- src/LibraryItem.cpp
+++ src/LibraryItem.cpp
@@ -6,10 +6,13 @@ LibraryItem::LibraryItem(long int id)
 : empty(false) { }
 
 LibraryItem::LibraryItem(const std::string& name, const std::string& author, const std::string& description, ItemKind itemKind)
-: id(id), name(name), author(author), description(description), itemKind(itemKind), empty(false) { }
+: LibraryItem(0, name, author, description, itemKind) { }
 
 LibraryItem::LibraryItem(long int id, const std::string& name, const std::string& author, const std::string& description, ItemKind itemKind)
-: id(id), name(name), author(author), description(description), itemKind(itemKind), empty(false), initialized(true) { }
+: id(id), name(name), author(author), description(description), itemKind(itemKind) {
+    empty = (id <= 0 && name.empty() && author.empty() && description.empty() && itemKind.isEmpty()) ? true : false;
+    initialized = (id >= 0 && !name.empty() && !author.empty() && !description.empty() && itemKind.isInitialized()) ? true : false;
+}
 
 long int LibraryItem::getId() const {
     return id;
