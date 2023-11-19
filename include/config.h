78e343e1d745e81b8ff70f3527f439c3ee218035
Gabriel GRONDIN <gglinnk@protonmail.com>
2023-11-19 20:34:58 +0100

    Update config header
    Defines APPDATAROOT representing default env variable name for user data files.

--- include/config.h
+++ include/config.h
@@ -3,3 +3,9 @@
 #define PROJECT_NAMEVER "LibraryManager 0.1.0"
 
 #define UI_DISABLED true
+
+#ifdef WIN32
+    #define APPDATAROOT "LOCALAPPDATA"
+#elif UNIX or __linux__
+    #define APPDATAROOT "HOME"
+#endif
